5c5f377025ed37dc734275dfbb07508721c6931b
Kiran Panesar <kiran@mobilexlabs.com>
2014-06-02 22:14:11 +0100

    Testing optionals and control statements

--- MyFirstPlayground.playground/section-1.swift
+++ MyFirstPlayground.playground/section-1.swift
@@ -18,10 +18,34 @@ let fancyConcatination = "The number \(explicitInteger) is greater than \(explic
 
 // Let's check out arrays
 var announcements = ["iOS 8", "Mac OS 10.10", "Metal", "SceneKit", "Swift"]
-announcements[8]
 
 // Let's check out dictionaries
 var devices = [
     "iphone5" : "ios 7",
     "ipad mini" : "ios 8 beta"
 ]
+
+// Control statements (for-in, if)
+for announcement in announcements {
+    if announcement == "Swift" {
+        var featureString = "We're testing \(announcement)"
+    }
+}
+
+// Checking out this whole optionals thing
+var optionalUsername :String? = "Kiran"
+
+// Let name equal the username, if name != nil, greet the person
+if let name = optionalUsername {
+    var greeting = "Hey, \(name)"
+}
+
+// nil the username
+optionalUsername = nil
+
+// Redo the above test, this time it will evaluate to false
+if let name = optionalUsername {
+    var greeting = "Hey, \(name)"
+} else {
+    var greeting = "Hey! I don't believe we've met!"
+}
