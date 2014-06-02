64e009199f5edb4910a5177b26aaf25795c20e95
Kiran Panesar <kiran@mobilexlabs.com>
2014-06-02 21:34:49 +0100

    Basic variables/typing

--- MyFirstPlayground.playground/section-1.swift
+++ MyFirstPlayground.playground/section-1.swift
@@ -1,5 +1,27 @@
-// Playground - noun: a place where people can play
+// My first Playground. Let's see how this goes...
 
 import Cocoa
 
-var str = "Hello, playground"
+// Define the constants
+let pi = 3.142
+let explicitDouble :Double = 3
+
+// Define the variables
+var implicitString = "Hello, playground"
+var explicitInteger :Integer = 34
+
+// Let's try concatination
+let testingStringConcatination = implicitString + " " + String(explicitDouble)
+
+// Fancier concatination
+let fancyConcatination = "The number \(explicitInteger) is greater than \(explicitDouble)"
+
+// Let's check out arrays
+var announcements = ["iOS 8", "Mac OS 10.10", "Metal", "SceneKit", "Swift"]
+announcements[8]
+
+// Let's check out dictionaries
+var devices = [
+    "iphone5" : "ios 7",
+    "ipad mini" : "ios 8 beta"
+]
